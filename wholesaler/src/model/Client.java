acd6f902cc5b4a0389213311a21db14dfd2afae8
Danjie Zhou <danjiezhou@hotmail.com>
2020-02-03 19:19:04 +0100

    version 0.02

--- wholesaler/src/model/Client.java
+++ wholesaler/src/model/Client.java
@@ -4,15 +4,47 @@ public class Client {
 	private int id;
 	private String lastname;
 	private String firstname;
+	private String adress;
+	private String country;
+	private String zipCode;
+	private String tel;
 	
-	public Client(int id, String lastname, String firstname) {
+
+	
+	public Client(int id, String lastname, String firstname, String adress, String country, String zipCode,
+			String tel) {
 		super();
 		this.id = id;
 		this.lastname = lastname;
 		this.firstname = firstname;
+		this.adress = adress;
+		this.country = country;
+		this.zipCode = zipCode;
+		this.tel = tel;
 	}
-	public Client() {
-		// TODO Auto-generated constructor stub
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
 	}
 	public int getId() {
 		return id;
