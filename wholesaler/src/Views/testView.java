4e72ed4b6c4bcb044d108382ae96cde437b00a63
Anis Toumi <t.anis@hotmail.be>
2020-04-06 16:56:30 +0200

    Addition of the views

---
+++ wholesaler/src/Views/testView.java
@@ -0,0 +1,44 @@
+package Views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+
+public class testView {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					testView window = new testView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public testView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setLayout(new GridLayout(3, 4, 1, 1));
+	}
+
+}
